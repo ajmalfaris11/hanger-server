287e4016e9849928208be690aa7711f4b038b181
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-02-23 12:00:00 +0530

    Add database connection setup before starting the server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const { app } = require(".");
+const { connectDb } = require("./config/db");
 
 const PORT= 4647;
-app.listen(PORT, ()=>{
+app.listen(PORT, async()=>{
+    await connectDb();
     console.log("Hanger E-commerce api listing on port : ",PORT)
 })
