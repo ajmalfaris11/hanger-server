415aab42dd1d356fb6a96c2bccca75360279a205
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-02-28 22:22:15 +0530

    Implement login functionality. Validates user credentials, checks password, and generates JWT token upon successful authentication.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const userService=require("../services/user.service.js")
 const jwtProvider=require("../config/jwtProvider.js")
+const bcrypt=require("bcrypt")
+const cartService=require("../services/cart.service.js")
 
 
 const register=async(req,res)=>{
@@ -16,5 +18,27 @@ const register=async(req,res)=>{
         return res.status(500).send({error:error.message})
     }
 }
+const login=async(req,res)=>{
+    const {password,email}=req.body
+    try {
+        const user = await userService.getUserByEmail(email);
+ 
+        if (!user) {
+            return res.status(404).json({ message: 'User not found With Email ', email});
+        }
 
-module.exports={register}
+        const isPasswordValid=await bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        const jwt=jwtProvider.generateToken(user._id);
+
+        return res.status(200).send({jwt,message:"login success"});
+
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+module.exports={register,login}
