a09feef7d58e8d7f1f8af0147079ff655592fe99
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-02 23:53:21 +0530

    Added getAllUsers controller to fetch all users from the database

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -18,5 +18,13 @@ const getUserProfile=async (req,res)=>{
     }
 }
 
+const getAllUsers=async(req,res)=>{
+    try {
+        const users=await userService.getAllUsers()
+        return res.status(200).send(users)
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
 
-module.exports={getUserProfile}
+module.exports={getUserProfile,getAllUsers}
