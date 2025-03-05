869a7b908766fcf08edd09a9d577adba55434ac3
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-06 01:12:19 +0530

    Add: New function to find and update user cart details with prices and discounts

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cart.model.js");
+const CartItem = require('path/to/cartItemModel');
 
 // Create a new cart for a user
 async function createCart(user) {
@@ -11,4 +12,37 @@ async function createCart(user) {
   }
 }
 
-module.exports = { createCart };
+// Find a user's cart and update cart details
+async function findUserCart(userId) {
+  try {
+    let cart =await Cart.findOne({ user: userId })
+  
+    let cartItems=await CartItem.find({cart:cart._id}).populate("product")
+  
+    cart.cartItems=cartItems;
+    
+  
+    let totalPrice = 0;
+    let totalDiscountedPrice = 0;
+    let totalItem = 0;
+  
+    for (const cartItem of cart.cartItems) {
+      totalPrice += cartItem.price;
+      totalDiscountedPrice += cartItem.discountedPrice;
+      totalItem += cartItem.quantity;
+    }
+  
+    cart.totalPrice = totalPrice;
+    cart.totalItem = totalItem;
+    cart.totalDiscountedPrice = totalDiscountedPrice;
+    cart.discounte = totalPrice - totalDiscountedPrice;
+  
+    // const updatedCart = await cart.save();
+    return cart;
+
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = { createCart, findUserCart };
