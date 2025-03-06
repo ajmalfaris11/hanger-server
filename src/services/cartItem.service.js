d91949ed532f622d2fc05ead1086b40542c233a2
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-07 02:31:47 +0530

    Add: Function to update an existing cart item with new quantity and price

--- src/services/cartItem.service.js
+++ src/services/cartItem.service.js
@@ -1,4 +1,5 @@
 const CartItem = require("../models/cartItem.model.js");
+const userService=require("../services/user.service.js");
 
 // Create a new cart item
 async function createCartItem(cartItemData) {
@@ -16,6 +17,33 @@ async function createCartItem(cartItemData) {
   }
 }
 
+// Update an existing cart item
+async function updateCartItem(userId, cartItemId, cartItemData) {
+  try {
+    const item = await findCartItemById(cartItemId);
 
+    if (!item) {
+      throw new Error("cart item not found : ", cartItemId);
+    }
+    const user = await userService.findUserById(item.userId);
 
-module.exports = { createCartItem };
+    if (!user) {
+      throw new Error("user not found : ", userId);
+    }
+
+    if (user.id === userId.toString()) {
+      item.quantity = cartItemData.quantity;
+      item.price = item.quantity * item.product.price;
+      item.discountedPrice = item.quantity * item.product.discountedPrice;
+
+      const updatedCartItem = await item.save();
+      return updatedCartItem;
+    } else {
+      throw new Error("You can't update another user's cart_item");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = { createCartItem, updateCartItem };
