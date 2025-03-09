1a5389e61e7339004f7c0af3809132929ec135e3
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-10 01:35:16 +0530

    `feat: add addItemToCart endpoint to add items to user cart`

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const cartService = require("../services/cart.service.js");
 
+// Retrieves the cart of the currently authenticated user.
 const findUserCart = async (req, res) => {
-    const user = req.user;
+  const user = req.user;
   try {
     const cart = await cartService.findUserCart(user.id);
-    res.status(200).json(cart);
+    res.status(201).json(cart);
   } catch (error) {
     // Handle error here and send appropriate response
     res
@@ -16,5 +17,21 @@ const findUserCart = async (req, res) => {
   }
 };
 
+// Adds an item to the authenticated user's cart.
+const addItemToCart = async (req, res) => {
+  const user = req.user;
+  try {
+    await cartService.addCartItem(user._id.toString(), req.body);
 
-module.exports = { findUserCart };
+    res
+      .status(201)
+      .json({ message: "Item Added To Cart Successfully", status: true });
+  } catch (error) {
+    // Handle error here and send appropriate response
+    res
+      .status(500)
+      .json({ message: "Failed to add item to cart.", error: error.message });
+  }
+};
+
+module.exports = { findUserCart, addItemToCart };
