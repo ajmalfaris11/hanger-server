a88f57ec6edfdf0143ff0d67d98d8949d6111104
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-10 00:58:15 +0530

    feat: add shipOrder endpoint to process order shipment

---
+++ src/controllers/adminOrder.controller.js
@@ -0,0 +1,25 @@
+const orderService = require("../services/order.service");
+
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await orderService.getAllOrders();
+    return res.status(202).send(orders);
+  } catch (error) {
+    res.status(500).send({ error: "Something went wrong" });
+  }
+};
+
+const shippOrder = (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+    const order = orderService.shipOrder(orderId);
+    return res.status(202).send(order);
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+module.exports = {
+  getAllOrders,
+  shippOrder,
+};
