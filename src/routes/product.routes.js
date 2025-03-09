613bca1394bb4e920f552d00fdffbe266a1982f1
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-10 04:15:09 +0530

    feat: add product search and retrieval endpoints with authentication

---
+++ src/routes/product.routes.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const router=express.Router();
+
+const productController=require("../controllers/product.controller.js");
+const authenticate = require("../middleware/authenticate.js");
+
+router.get('/', authenticate, productController.getAllProducts);
+router.get('/id/:id', authenticate, productController.findProductById);
+router.get('/search', authenticate, productController.searchProduct);
+
+
+module.exports = router;
