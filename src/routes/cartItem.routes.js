a5e27445e0e776a0712a93c2500a0d8ff4832274
Ajmal Faris <ajmalfaris.me@gmail.com>
2025-03-10 03:47:54 +0530

    feat: add routes for updating and removing cart items with authentication

---
+++ src/routes/cartItem.routes.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const router=express.Router();
+
+const authenticate = require("../middleware/authenticate.js");
+const cartItemController=require("../controllers/cartItem.controller.js");
+
+
+router.put("/:id",authenticate,cartItemController.updateCartItem);
+router.delete("/:id",authenticate,cartItemController.removeCartItem);
+
+module.exports=router;
