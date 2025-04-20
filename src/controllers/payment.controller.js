a72131d6bb340dcc0e21059e4447f0687727ad5d
ajmal faris <ajmalfaris.me@gmail.com>
2025-04-20 20:23:12 +0530

    feat: implement updatePaymentInformation controller to update order status based on Razorpay payment details

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -11,4 +11,15 @@ const createPaymentLink=async(req,res)=>{
 
 }
 
-module.exports={createPaymentLink}
+const updatePaymentInformation=async(req,res)=>{
+
+    try {
+        await paymentService.updatePaymentInformation(req.query)
+        return res.status(200).send({message:"payment information updated",status:true})
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+
+}
+
+module.exports={createPaymentLink,updatePaymentInformation}
