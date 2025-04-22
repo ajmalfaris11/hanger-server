e988d8a9c3f72b931af07f9fe12c8be59a5f5e60
ajmal faris <ajmalfaris.me@gmail.com>
2025-04-23 01:39:33 +0530

    bcrypt updated to bcryptjs

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user.model.js");
 const jwtProvider = require("../config/jwtProvider");
 
